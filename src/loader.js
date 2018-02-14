4b89114209419c74f9b82222e663002c1dbd7883
mainsim <33512485+mainsim@users.noreply.github.com>
2018-02-14 11:50:38 +0100

    Indentation optimized

--- src/loader.js
+++ src/loader.js
@@ -118,7 +118,7 @@ class loader {
 	 * @param {object} val - Promised class
 	 * @returns void
 	 */
-    static define(obj, prop, val) {
+    	static define(obj, prop, val) {
 		Object.defineProperty(obj, prop, {
 			value: val
 		})
